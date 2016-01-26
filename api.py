c9eea5c469b662ee9f7f1854c55cb93a3c5c92fd
Marius Avram <mariusavram91@gmail.com>
2016-01-26 23:52:37 +0000

    added pdfkit

--- api.py
+++ api.py
@@ -1,9 +1,12 @@
 from flask import Flask
+import pdfkit
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
+    config = pdfkit.configuration(wkhtmltopdf='/usr/bin/wkhtmltopdf')
+    pdfkit.from_url('http://google.com', 'out.pdf', configuration=config)
     return "Hello, World!"
 
 if __name__ == '__main__':
