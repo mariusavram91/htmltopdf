b9ed7cc36b1a224bbca83d7b09eb4de8a824c93d
Marius Avram <mariusavram91@gmail.com>
2016-01-26 23:24:02 +0000

    added tests.py

---
+++ tests.py
@@ -0,0 +1,10 @@
+import os
+import unittest
+
+class MainTestCase(unittest.TestCase):
+    def setUp(self):
+        self.url = ""
+        self.pdf = ""
+
+if __name__ == '__main__':
+    unittest.main()
