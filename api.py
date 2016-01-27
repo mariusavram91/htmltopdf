8479a9246c5e258d80436305f4caa513ff42b4e7
Marius Avram <mariusavram91@gmail.com>
2016-01-27 02:23:47 +0000

    added download pdf

--- api.py
+++ api.py
@@ -9,6 +9,16 @@ def index():
     #pdfkit.from_url('http://google.com', 'tmp/out.pdf', configuration=config)
     return "Hello, World!"
 
+@app.route('/download')
+def index():
+    config = pdfkit.configuration(wkhtmltopdf='bin/wkhtmltopdf')
+    file_name = 'out.pdf'
+    pdfkit.from_url('http://google.com', 'tmp/' + file_name, configuration=config)
+    headers = {"Content-Disposition": "attachment; filename=%s" % file_name}
+    with open('tmp/' + file_name, 'r') as f:
+        body = f.read()
+    return make_response((body, headers))
+
 @app.route('/download_csv')
 def download():
     csv = """"REVIEW_DATE","AUTHOR","ISBN","DISCOUNTED_PRICE"
