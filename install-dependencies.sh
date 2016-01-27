1b566d88f7678ac828c03f9b1990dca0e22dce53
Marius Avram <mariusavram91@gmail.com>
2016-01-27 01:05:44 +0000

    xvfb added to dependencies

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -1,5 +1,8 @@
 !#/bin/sh
+apt-get update
+apt-get upgrade
+apt-get install xvfb
+apt-get install xfonts-75dpi
 wget http://download.gna.org/wkhtmltopdf/0.12/0.12.2/wkhtmltox-0.12.2_linux-trusty-amd64.deb
 dpkg -i wkhtmltox-0.12.2_linux-trusty-amd64.deb
-apt-get install xfonts-75dpi
 rm wkhtmltox-0.12.2_linux-trusty-amd64.deb
