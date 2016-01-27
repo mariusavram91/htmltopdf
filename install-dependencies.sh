f52d266f56b494c36dc2f6df12774c22f26703e4
Marius Avram <mariusavram91@gmail.com>
2016-01-27 01:09:08 +0000

    removed apt-get upgrade

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -1,8 +1,7 @@
 !#/bin/sh
 apt-get update
-apt-get upgrade
-apt-get install xvfb
-apt-get install xfonts-75dpi
+apt-get -y install xvfb
+apt-get -y install xfonts-75dpi
 wget http://download.gna.org/wkhtmltopdf/0.12/0.12.2/wkhtmltox-0.12.2_linux-trusty-amd64.deb
 dpkg -i wkhtmltox-0.12.2_linux-trusty-amd64.deb
 rm wkhtmltox-0.12.2_linux-trusty-amd64.deb
